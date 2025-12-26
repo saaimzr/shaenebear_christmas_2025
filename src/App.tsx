1a2bdec4e82db75eaf104c6a7b9dba9c993cd15d
saaimzr <saaim.raad3@gmail.com>
2025-12-26 17:59:12 -0500

    changed card

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,7 @@ type BirthdayCardConfig = {
 const BIRTHDAY_CARDS: ReadonlyArray<BirthdayCardConfig> = [
   {
     id: "confetti",
-    image: "/ebear_card2.png",
+    image: "/ebear_card_final.png",
     position: [1, 0.081, -2],
     rotation: [-Math.PI / 2, 0, Math.PI / 3],
   }
